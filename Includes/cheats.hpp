e2961124aa81e2f63c8df59f3764daf77da030d5
hackNcode <97558900+hackNcode@users.noreply.github.com>
2022-01-14 12:48:47 -0800

    Add files via upload

---
+++ Includes/cheats.hpp
@@ -0,0 +1,21 @@
+#ifndef CHEATS_H
+#define CHEATS_H
+
+#include <CTRPluginFramework.hpp>
+#include "Helpers.hpp"
+#include "Unicode.h"
+
+namespace CTRPluginFramework
+{
+    using StringVector = std::vector<std::string>;
+
+    void invincible(MenuEntry *entry);
+    void alwaysStarPower(MenuEntry *entry);
+    void alwaysBlackKart(MenuEntry *entry);
+    void alwaysShocked(MenuEntry *entry);
+    void alwaysCrushed(MenuEntry *entry);
+    void trickAnywhere(MenuEntry *entry);
+    void trulyRandomItems(MenuEntry *entry);
+
+}
+#endif
