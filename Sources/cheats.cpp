01bdc12d9709839bc278e7bb18f3bdfda6394e7a
hackNcode <97558900+hackNcode@users.noreply.github.com>
2022-01-15 12:03:59 -0800

    Add files via upload

---
+++ Sources/cheats.cpp
@@ -0,0 +1,236 @@
+#include "cheats.hpp"
+#include "CTRPluginFramework.hpp"
+#include "Unicode.h"
+#include "csvc.h"
+#include "3ds.h"
+#include <math.h>
+#include <vector>
+
+namespace CTRPluginFramework
+{
+    using StringVector = std::vector<std::string>;
+    u32 offset = 0, data = 0;
+    static u32 original_speed[0x2D];
+
+	/////////////////////////////////////////////////////////    Start of custom functions    /////////////////////////////////////////////////////////
+
+	/*void	dumpFrames(MenuEntry *entry)
+	{
+		File file;
+		Directory dir("frames", true);
+		int frame;
+		for (int x = 0; x < 400; x++)
+		{
+			for (int y = 0; y < 240; y++)
+			{
+				Color c;
+				ReadPixel(x, y, c, false);
+				*(unsigned int)(0x18000000 + (4 * x) + (4 * y)) = c.ToU32();
+			}
+		}
+		if (dir.OpenFile(file, "frame" + to_string(frame) + ".bmp", File::RWC) == 0)
+		{
+			file.Dump(0x18000000, 384000);
+			frame++;
+		}
+	}*/
+	
+	bool is_in_range(u32 value, u32 lowerbound, u32 upperbound)
+	{
+		return value > lowerbound && value < upperbound;
+	}
+
+	bool IsInRace()
+	{
+		if (Process::Read32(0x14000084, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			if (Process::Read32(offset + 0x316C, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+				return Process::Read32(offset + 0x118, offset) && (offset & 0xFF);
+		return 0;
+	}
+
+	u32 GetRacePointer()
+	{
+		if (IsInRace())
+			if (Process::Read32(0x140002F4, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+				if (Process::Read32(offset + 0x14, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+					if (Process::Read32(offset + 0x518, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+						if (Process::Read32(offset + 0x1C, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+							return offset;
+		return 0;
+	}
+
+	u32	GetFNsPointer()
+	{
+		if (IsInRace() && Process::Read32(0xFFFF6F0, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			return offset + 8;
+		return 0;
+	}
+
+	u32	GetOldPointer5CC()
+	{
+		if (IsInRace() && Process::Read32(0xFFFFBF4, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			if (Process::Read32(offset + 0x5CC, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+				return offset;
+		return 0;
+	}
+
+	u32	GetOldPointer5D0()
+	{
+		if (IsInRace() && Process::Read32(0xFFFFBF4, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			if (Process::Read32(offset + 0x5D0, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+				return offset;
+		return 0;
+	}
+
+	u32	GetItemPointer()
+	{
+		if (IsInRace() && Process::Read32(0x14000074, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			if (Process::Read32(offset - 0x1B5C, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+				if (Process::Read32(offset + 0x27AC, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+					return offset;
+		return 0;
+	}
+
+	u8 GetTotalPlayers()
+	{
+		u8 total;
+		if (Process::Read32(0x14000074, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			if (Process::Read32(offset - 0xC10, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+				if (Process::Read32(offset + 0x23C, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+					if (Process::Read8(offset - 0xF28, total) && is_in_range(offset, 0x14000000, 0x18000000))
+						return total;
+		return 0;
+	}
+
+	u16	GetTime()
+	{
+		u16 time = 0;
+		if (GetOldPointer5D0())
+		{
+			Process::Read16(GetOldPointer5D0() + 0x484, time);
+			time /= 60;
+			if (300 - time < 0)
+				return 0;
+			else
+				return 300 - time;
+		}
+		return 0;
+	}
+
+	u16	GetMinutes()
+	{
+		u16 time = GetTime(), minutes = 0;
+		while (time - 60 >= 0)
+		{
+			time -= 60;
+			minutes++;
+		}
+		return minutes;
+	}
+
+	u16	GetSeconds()
+	{
+		u16 time = GetTime();
+		while (time - 60 >= 0)
+			time -= 60;
+		return time;
+	}
+
+	void	SubToTime(u16 seconds)
+	{
+		u16 time = 0;
+		if (offset)
+		{
+			Process::Read16(GetOldPointer5D0() + 0x484, time);
+			time -= (seconds * 60);
+			Process::Write16(GetOldPointer5D0() + 0x484, time);
+			Process::Write16(GetRacePointer() + 0xC4, time);
+		}
+	}
+
+	void	writeItem(u32 item)
+	{
+		if (GetItemPointer())
+		{
+			Process::Write32(GetItemPointer() + 0x3C, 0xFFFFFFFF);
+			Process::Write32(GetItemPointer() + 0xA8, 3);
+			Process::Write32(GetItemPointer() + 0xC8, item);
+			Process::Write32(GetItemPointer() + 0xD8, 0x3F800000);
+		}
+	}
+
+	void	writeSpeed(u32 speed)
+	{
+		for (int i = 0; i < 0x2D; i++)
+		{
+			if (Process::Read32(0x140002F4, offset) && is_in_range(offset, 0x14000000, 0x18000000) && Process::Read32(offset - 0xA4, offset) && is_in_range(offset, 0x14000000, 0x18000000))
+			{
+				if (original_speed[2] != speed)
+					Process::Read32((offset - 0x2C3B0) + (i * 4), original_speed[i]);
+				Process::Write32((offset - 0x2C3B0) + (i * 4), speed);
+				Process::Write32((offset - 0x28E90) + (i * 4), speed);
+				Process::Write32((offset - 0x1C730) + (i * 4), speed);
+			}
+		}
+	}
+
+	void	writeVR(u32 vr)
+	{
+		if (Process::Read32(0x663D04, offset) && is_in_range(offset, 0x10000000, 0x18000000))
+			Process::Write32(offset - 0xE30, vr);
+	}
+
+	void	writeLocation(u32 location)
+	{
+		if (Process::Read32(0x6673C8, offset) && is_in_range(offset, 0x10000000, 0x18000000))
+			Process::Write32(offset + 0xF7CC, location);
+	}
+
+	void	writeFlag(u16 flag)
+	{
+		if (Process::Read32(0x6673C8, offset) && is_in_range(offset, 0x10000000, 0x18000000))
+		{
+			Process::Write8(offset + 0xE7CA, flag);
+			Process::Write8(offset + 0xF7D0, flag);
+		}
+	}
+	void invincible(MenuEntry *entry)
+	{
+		if (IsInRace())
+			Process::Write16(GetRacePointer() + 0x102C, 0xFFFF);
+	}
+	void alwaysStarPower(MenuEntry *entry)
+	{
+		if (IsInRace())
+			Process::Write16(GetRacePointer() + 0xFF4, 0xFFFF);
+	}
+	void alwaysBlackKart(MenuEntry *entry)
+	{
+		if (IsInRace())
+			Process::Write16(GetRacePointer() + 0xFF8, 0xFFFF);
+	}
+
+	void alwaysShocked(MenuEntry *entry)
+	{
+		if (IsInRace())
+			Process::Write16(GetRacePointer() + 0x1000, 0xFFFF);
+	}
+
+	void alwaysCrushed(MenuEntry *entry)
+	{
+		if (IsInRace())
+			Process::Write16(GetRacePointer() + 0x1000, 0xFFFF);
+	}
+	void trickAnywhere(MenuEntry *entry)
+	{
+		if (IsInRace())
+			Process::Write16(GetRacePointer() + 0xFF0, 0xFFFF);
+	}
+	void    randomItems(MenuEntry *entry)
+	{
+		u32 number = Utils::Random(0, 0x14);
+		writeItem(number == 0xF || number == 0x10 ? Utils::Random(0, 0xE) : number);
+	}
+
+
+}
---
+++ Sources/main.cpp
@@ -0,0 +1,155 @@
+#include <3ds.h>
+#include "csvc.h"
+#include <CTRPluginFramework.hpp>
+
+#include "cheats.hpp"
+#include <vector>
+
+namespace CTRPluginFramework
+{
+    // This patch the NFC disabling the touchscreen when scanning an amiibo, which prevents ctrpf to be used
+
+        static MenuEntry *EntryWithHotkey(MenuEntry *entry, const std::vector<Hotkey> &hotkeys)
+    {
+        if (entry != nullptr)
+        {
+            for (const Hotkey &hotkey : hotkeys)
+                entry->Hotkeys += hotkey;
+        }
+
+        return (entry);
+    } 
+
+
+
+    static void    ToggleTouchscreenForceOn(void)
+    {
+        static u32 original = 0;
+        static u32 *patchAddress = nullptr;
+
+        if (patchAddress && original)
+        {
+            *patchAddress = original;
+            return;
+        }
+
+        static const std::vector<u32> pattern =
+        {
+            0xE59F10C0, 0xE5840004, 0xE5841000, 0xE5DD0000,
+            0xE5C40008, 0xE28DD03C, 0xE8BD80F0, 0xE5D51001,
+            0xE1D400D4, 0xE3510003, 0x159F0034, 0x1A000003
+        };
+
+        Result  res;
+        Handle  processHandle;
+        s64     textTotalSize = 0;
+        s64     startAddress = 0;
+        u32 *   found;
+
+        if (R_FAILED(svcOpenProcess(&processHandle, 16)))
+            return;
+
+        svcGetProcessInfo(&textTotalSize, processHandle, 0x10002);
+        svcGetProcessInfo(&startAddress, processHandle, 0x10005);
+        if(R_FAILED(svcMapProcessMemoryEx(CUR_PROCESS_HANDLE, 0x14000000, processHandle, (u32)startAddress, textTotalSize)))
+            goto exit;
+
+        found = (u32 *)Utils::Search<u32>(0x14000000, (u32)textTotalSize, pattern);
+
+        if (found != nullptr)
+        {
+            original = found[13];
+            patchAddress = (u32 *)PA_FROM_VA((found + 13));
+            found[13] = 0xE1A00000;
+        }
+
+        svcUnmapProcessMemoryEx(CUR_PROCESS_HANDLE, 0x14000000, textTotalSize);
+exit:
+        svcCloseHandle(processHandle);
+    }
+
+    // This function is called before main and before the game starts
+    // Useful to do code edits safely
+    void    PatchProcess(FwkSettings &settings)
+    {
+        ToggleTouchscreenForceOn();
+    }
+
+    // This function is called when the process exits
+    // Useful to save settings, undo patchs or clean up things
+    void    OnProcessExit(void)
+    {
+        ToggleTouchscreenForceOn();
+    }
+
+    void    InitMenu(PluginMenu &menu)
+    {
+        // Create your entries here, or elsewhere
+        // You can create your entries whenever/wherever you feel like it
+        
+        // Example entry
+        /*menu += new MenuEntry("Test", nullptr, [](MenuEntry *entry)
+        {
+            std::string body("What's the answer ?\n");
+
+            body += std::to_string(42);
+
+            MessageBox("UA", body)();
+        });*/
+    }
+
+    
+
+ int main()
+    {
+        if (Process::GetTitleID() == 0x0004000000030800)
+        {
+            PluginMenu *menu = new PluginMenu(Color::Green << "Mario Kart 7", 1, 0, 0, "Plugin For MK7");
+            menu->SynchronizeWithFrame(true);
+            
+            const std::vector<MenuEntry *> Battle {
+
+            new MenuEntry("Invincible", invincible),
+            new MenuEntry("Always Star Power", alwaysStarPower),
+            new MenuEntry("Random Items", randomItems),
+ 
+            };
+
+            menu->Append(new MenuFolder("Battle", "Use Battle Cheats", Battle));
+
+            const std::vector<MenuEntry *> Kart{
+
+            new MenuEntry("Always Black Kart", alwaysBlackKart),
+            new MenuEntry("Always Shocked", alwaysShocked),
+            new MenuEntry("Always Crushed", alwaysCrushed),
+            EntryWithHotkey(new MenuEntry("Trick Jump (R)", trickAnywhere, "Trick Jump (R)"), { Hotkey(Key::R, "Trick Jump (R)") }),
+            };
+            menu->Append(new MenuFolder("Kart", "Use Kart Cheats", Kart));
+
+
+            
+            
+
+
+            menu->Run();
+            delete menu;
+        }
+        else {
+            MessageBox("This game is not supported.")();
+            PluginMenu *menu = new PluginMenu("This Game Is Unsupported", 0, 0, 0, "Unsupported");
+            menu->SynchronizeWithFrame(true);
+            menu->Run();
+            delete menu;
+            return (0);
+        }
+
+        // Exit plugin
+        
+    }
+}
+    
+
+
+
+                
+                
